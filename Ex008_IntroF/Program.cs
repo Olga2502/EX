731318f71e6e04af778e85e74d08e79e6fe8e903
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-10 22:36:25 +0300

    Более упростили программу с функцией

--- Ex008_IntroF/Program.cs
+++ Ex008_IntroF/Program.cs
@@ -10,15 +10,22 @@ int b1 = 21;
 int c1 = 39;
 int a2 = 12;
 int b2 = 23;
-int c2 = 313;
+int c2 = 3123;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
 
-
+ Max(a1, b1, c1);
+ Max(a2, b2, c2);
+ Max(a3, b3, c3);
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+    
 Console.WriteLine(max);
